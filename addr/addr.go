ae61900f3662be97826ac4be64d286f1a3ba2377
Simon Gombrii <simon@gombrii.se>
2025-09-24 20:16:13 +0200

    addr in own package

--- addr/addr.go
+++ addr/addr.go
@@ -1,4 +1,4 @@
-package e2e
+package addr
 
 import (
 	"fmt"
@@ -17,7 +17,7 @@ func SetAddressBook(book AddressBook) {
 	addrs = book
 }
 
-func Addr(svc string) string {
+func Lookup(svc string) string {
 	if os.Args[1] == "" {
 		fmt.Printf("No env arg provided. Needed to run tests containing AddressBook lookups.\n")
 		os.Exit(badArgument)
@@ -34,7 +34,7 @@ func Addr(svc string) string {
 	return addr
 }
 
-func EnvAddr(env, svc string) string {
+func EnvLookup(env, svc string) string {
 	if addr, ok := addrs[env][svc]; !ok {
 		panic(fmt.Sprintf("Attempt access address for combination of env %q and svc %q that does not exist", env, svc))
 	} else {
